c31b5b91b291fae58d1ea8a5d25547570d8755d6
luis-moguea <luis.mogueadp@gmail.com>
2022-12-24 12:58:52 -0500

    js updates

--- click.js
+++
@@ -1,7 +0,0 @@
-onClick.forEach(el => {
-    el.addEventListener("click", () => {
-        el.querySelector(".main-js__content").classList.toggle("inactive")
-        el.querySelector(".unseen").classList.toggle("seen")
-        el.querySelector(".main-js__message--text").classList.toggle("active-text")
-    })
-})
